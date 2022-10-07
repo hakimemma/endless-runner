8c08b195ec86991e4a628393c3fb0a862591e3f3
hakimemma <113105266+hakimemma@users.noreply.github.com>
2022-10-07 15:36:33 +0100

    spawner

--- Assets/cameraconrol.cs
+++ Assets/cameraconrol.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class cameraconrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //create a public referrence to the player - we will assign this using the unity editor
+    public  GameObject player;
+
+    void Update()
     {
+        //change our position relative to the players position
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
